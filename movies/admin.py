0826aab98f26d373e8c51c54cad3ddab2100b4ad
ErrorInever <errorinever@gmail.com>
2020-10-21 22:12:55 +0000

    add movies

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Reviews, RatingStar, Category, MovieShots, Actor, Genre, Movie, Rating
 
-# Register your models here.
+
+admin.site.register(Reviews)
+admin.site.register(RatingStar)
+admin.site.register(Category)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(Rating)
+
+
